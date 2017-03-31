3932be229114ea284bb07cf414eab80388f4f739
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-03-31 10:09:42 +0200

    CreateTables/DeleteTables Fns V2, added .commit()

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def makeWebhookResult(req):
         return requestUserName(req)
     elif req.get("result").get("action") == "createDB":
         conn = Database.Database()
-        return conn.__createTables__(conn.connection)
+        return conn.__createTables__()
     else:
         return {}
 
