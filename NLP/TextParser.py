106427597f1ecbf616994ba67a85b079df4b04af
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-04-10 20:29:33 +0200

    Code organisation 2.0

--- NLP/TextParser.py
+++ NLP/TextParser.py
@@ -1,15 +1,4 @@
 from nltk.tokenize import word_tokenize
 
 class TextParser:
-
-    def parse(input):
-        def tokenize(input):
-            list = word_tokenize(input)
-            return list
-
-        def extractKeywords(list):
-            stopList = set(line.strip() for line in open('stoplist'))
-            filtered_words = [word for word in list if word not in stopList]
-            return filtered_words
-
-        return extractKeywords(tokenize(input))
+    #dummy_data = "dummuy"
