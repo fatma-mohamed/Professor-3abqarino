f84f919d19e9686349f23233d857a7b86901834f
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-04-10 23:15:04 +0200

    getRandomQuestion with Code organization V2

--- Data/DataAccess.py
+++ Data/DataAccess.py
@@ -1,2 +1,23 @@
 class DataAccess:
-    dummy_data="dummy"
+
+    def getRandomQuestion(self):
+        conn = Database.Database()
+        cur = conn.cursor()
+        print "--------Getting a random Question--------"
+        cur.execute("SELECT * FROM "Questions_Answers" OFFSET floor(random()*(SELECT COUNT(*) FROM "Questions_Answers")) LIMIT 1")
+        rows = cur.fetchall()
+
+        for row in rows:
+            Question = row[0]
+            print "Question = ", Question, "\n"
+            A1 = row[1]
+            print "A1 = ", A1, "\n"
+            A2 = row[2]
+            print "A2 = ", A2, "\n"
+            A3 = row[3]
+            print "A3 = ", A3, "\n"
+            CA_ID = row[4]
+            print "CA_ID = ", CA_ID, "\n"
+        print "--------Got question--------"
+
+        return rows
--- ResponseSelection/FeatureTwoSelector.py
+++ ResponseSelection/FeatureTwoSelector.py
@@ -1,5 +1,25 @@
 from ResponseSelection.ResponseSelector import ResponseSelector
-
+from Data import DataAccess
 
 class FeatureTwoSelector(ResponseSelector):
-    dummy_data = "dummy"
+    
+    def getRandomQuestion(self):
+        row = DataAccess.getRandomQuestion()
+        
+        return {
+        "speech" : "",
+        "displayText": "",
+        "data": {},
+        "contextOut": [],
+        "source": "get-random-question",
+        "followupEvent":{
+            "name":"Question_Answers",
+            "data":{
+                "Question":row[0],
+                "A1":row[1],
+                "A2":row[2],
+                "A3":row[3],
+                "CA_ID":row[4]
+                }
+            }
+        }
