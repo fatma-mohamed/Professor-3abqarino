c9473bca08d5bc1cb8eccc39bfba429fffa78528
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-04-17 13:57:14 +0200

    alter table Questions_Answers

--- Data/DataAccess.py
+++ Data/DataAccess.py
@@ -5,6 +5,7 @@ import psycopg2
 class DataAccess:
     def insertQuestion_Answers(self, Question, A1, A2, A3, CA_ID):
         conn = Database.Database()
+        conn.alterTable_Questions_Answers()
         cur = conn.connection.cursor()
         print "--------Adding question to DB--------"
         cur.execute('''INSERT INTO "Questions_Answers" (Question, Answer_1, Answer_2, Answer_3, Correct_AnswerID) VALUES (''' + "'" + Question + "'" + ", " + "'" + A1 + "'" + ", " + "'" + A2 + "'" + ", " + "'" + A3 + "'" + ", " + (str)(CA_ID) + ")" + "ON CONFLICT (Question) DO NOTHING")
