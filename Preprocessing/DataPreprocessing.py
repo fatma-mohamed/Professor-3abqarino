337b97a0accbbfa47f117c877b1307e13d041c72
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-04-17 11:33:59 +0200

    insertQuestions_Answers() :: testing V5.2 - Modifying preprocessing - reading file

--- Preprocessing/DataPreprocessing.py
+++ Preprocessing/DataPreprocessing.py
@@ -19,7 +19,8 @@ class DataPreprocessing:
         ##rest of the preprocessing
 
     def insertQuestions_Answers(self):
-        f = open("Question_Answers.txt", 'r')
+        ff = open("requirements.txt", 'r')
+        f = open('''Question_Answers.txt''', 'r')
         i=0
         while True:
             Question = f.readline().rstrip()
