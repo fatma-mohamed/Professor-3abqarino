d762ccfe3ce3c03985900c84d1cb4a4a6a283cf4
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-04-17 15:42:31 +0200

    getRandomQuestion() :: test V1.2

--- ResponseSelection/FeatureTwoSelector.py
+++ ResponseSelection/FeatureTwoSelector.py
@@ -5,21 +5,5 @@ class FeatureTwoSelector:
     
     def getRandomQuestion(self):
         row = DataAccess.DataAccess().getRandomQuestion()
+        print "------------" + row + "---------"
         
-        return {
-        "speech" : "",
-        "displayText": "",
-        "data": {},
-        "contextOut": [],
-        "source": "get-random-question",
-        "followupEvent":{
-            "name":"Question_Answers",
-            "data":{
-                "Question":row[1],
-                "A1":row[2],
-                "A2":row[3],
-                "A3":row[4],
-                "CA_ID":row[5]
-                }
-            }
-        }
