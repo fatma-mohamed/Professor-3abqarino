eb3bbea282856bbe44afdbb315f1e44a9f3b3817
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-04-19 01:21:26 +0200

    Select function

--- Data/DataAccess.py
+++ Data/DataAccess.py
@@ -1,2 +1,15 @@
+from Data import Database
+
 class DataAccess:
-    dummy_data="dummy"
+
+    def select(self, table_name, cols, condition, value):
+        conn = Database.Database()
+        cur = conn.connection.cursor()
+        if (condition == ""):
+            cur.execute("SELECT ( " + cols + " ) from " + table_name)
+        else:
+            cur.execute("SELECT ( " + cols + " ) from " + table_name + " WHERE " + condition + " = " + value)
+
+        rows = cur.fetchall()
+        conn.close()
+        return rows
