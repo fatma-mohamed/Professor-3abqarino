c1033bb993c0cf3f1c4fd75babb472048589f8c0
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-04-19 22:55:38 +0200

    Preprocessing: insertAnswers_and_keywords

--- Preprocessing/DataPreprocessing.py
+++ Preprocessing/DataPreprocessing.py
@@ -21,12 +21,15 @@ class DataPreprocessing:
                 print("no answer!")
                 break
             answer = line
+            print("Question: ", question , "\nAnswer: ", answer)
             answer_id = Database.insert("Answers", "answer", answer)
             tokens = TextParser.tokenize(question)
             keywords = TextParser.removeStopWords(tokens)
             keywords_id = []
             for k in keywords:
+                print("Keyword: " , k)
                 keywords_id.append(Database.insert("Keywords", "keyword", k))
             for id in keywords_id:
+                print("K_ID: ", id)
                 v = (str)(answer_id) + "," + (str)(id)
                 Database.insert("Answers_Keywords", "answer_id, keyword_id", v)
