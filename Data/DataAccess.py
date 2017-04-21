edde1407f5a8a46b4cde0082a3fd4c289c8aceeb
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-04-21 21:16:28 +0200

    Preprocessing: insertAnswers_and_keywords

--- Data/DataAccess.py
+++ Data/DataAccess.py
@@ -1,2 +1,26 @@
+from Data import Database
+
 class DataAccess:
-    dummy_data="dummy"
+    def select(self, table_name, cols, condition, value):
+        conn = Database.Database()
+        cur = conn.connection.cursor()
+        if (condition == ""):
+            cur.execute("SELECT ( " + cols + ''' ) from "''' + table_name + '''"''')
+        else:
+            cur.execute("SELECT ( " + cols + ''' ) from "''' + table_name + '''" WHERE ''' + condition + " = " + value)
+
+        rows = cur.fetchall()
+        conn.close()
+        return rows
+
+    def select(self, table_name, cols, condition1, value1, condition2, value2):
+        conn = Database.Database()
+        cur = conn.connection.cursor()
+        if (condition1 == ""):
+            cur.execute("SELECT ( " + cols + ''' ) from "''' + table_name + '''"''')
+        else:
+            cur.execute("SELECT ( " + cols + ''' ) from "''' + table_name + '''" WHERE ''' + condition1 + " = " + value1 + " AND " + condition2 + " = " + value2)
+
+        rows = cur.fetchall()
+        conn.close()
+        return rows
