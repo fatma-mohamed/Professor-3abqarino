75d0d483b723442c6f40ac0a8ef5b4c56e31b548
Ibrahim Mostafa <Ibrahim Mostafa>
2017-04-23 01:57:27 +0200

    handle errors

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def makeWebhookResult(req):
         answer =  responseSelector.getAnswer()
         return {
 
-            "speech": "answer",
+            "speech": answer,
             "source": "prof-3abqarino_webhook",
             "displayText": "kjjkhjh"
 
