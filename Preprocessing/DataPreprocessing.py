ffb0ea72b57026a419aca4e19d49a9f133a33fe0
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-04-22 12:54:09 +0200

    Preprocessing: insertAnswers_and_keywords

--- Preprocessing/DataPreprocessing.py
+++ Preprocessing/DataPreprocessing.py
@@ -32,7 +32,7 @@ class DataPreprocessing:
             for k in keywords:
                 print("Keyword: " , k)
                 keyword_id = db_access.select("Keywords", "id", "keyword", "'"+k+"'")
-                if(keyword_id == None):
+                if(keyword_id == '[]'):
                     id = db.insert("Keywords", "keyword", "'"+k+"'", "keyword", "")
                     keywords_id.append(id)
                     synonyms = recognizer.getSynonym(k)
