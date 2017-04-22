40fde8c1a437c6a1d5b0996e70f1c0a7d1e9f011
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-04-22 22:39:45 +0200

    getAnswer function

--- ResponseSelection/FeatureOneSelector.py
+++ ResponseSelection/FeatureOneSelector.py
@@ -3,48 +3,46 @@ from NLP import *
 from Data import DataAccess
 from collections import Counter
 
+
 class FeatureOneSelector(ResponseSelector):
-    question=""
-    def __init__(self,question):
-        self.question=question
+    question = ""
+
+    def __init__(self, question):
+        self.question = question
 
     def getAnswer(self):
         t = TextParser.tokenize(self.question)
         k = TextParser.extractKeywords(t)
-        keywordsID=self.retriveSynonymID(k)
-       # ner = WordRecognizer.namedEntity(k)
+        keywordsID = self.retriveSynonymID(k)
+        # ner = WordRecognizer.namedEntity(k)
         mostCommenAnswers = self.retriveAnswersID(keywordsID)
-        if len(mostCommenAnswers) ==0:
+        if len(mostCommenAnswers) == 0:
             return False
         answer = self.retriveAnswer(mostCommenAnswers)
-        return  answer
+        return answer
 
-
-
-
-    def retriveSynonymID(self,keywords):
-        synonymKey=[]
+    def retriveSynonymID(self, keywords):
+        synonymKey = []
         for word in keywords:
             Da = DataAccess.DataAccess()
-            ids =  Da.select("Synonyms","key_id","synonym ="+word)
-            synonymKey +=ids
+            ids = Da.select("Synonyms", "key_id", "synonym =" + word)
+            synonymKey += ids
             # for  id  in ids:
             #     keyWord =Da.select("Keywords","keyword","id = "+id)
             #     synonymKey.append(keyWord)
 
         return synonymKey
 
-
-    def retriveAnswersID(self,keywordsIDs):
+    def retriveAnswersID(self, keywordsIDs):
         Da = DataAccess.DataAccess()
-        answersID=[]
+        answersID = []
         for id in keywordsIDs:
-            ids = Da.select("Answers_Keywords","answer_id","keyword_id = "+id)
-            answersID +=ids
+            ids = Da.select("Answers_Keywords", "answer_id", "keyword_id = " + id)
+            answersID += ids
 
         return Counter(answersID).most_common(3)
 
-    def retriveAnswer(self,IDs):
+    def retriveAnswer(self, IDs):
         Da = DataAccess.DataAccess()
-        Answer = Da.select("Answers","Answers","id = "+IDs[0][0])
+        Answer = Da.select("Answers", "Answers", "id = " + IDs[0][0])
         return Answer
