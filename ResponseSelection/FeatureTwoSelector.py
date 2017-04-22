6f0acd3bd6fc3d8a680c96e927225b68f7608eda
HodaFouad <hoda.fouad.saad@gmail.com>
2017-04-22 20:33:06 +0200

    "check answer correctness 11"

--- ResponseSelection/FeatureTwoSelector.py
+++ ResponseSelection/FeatureTwoSelector.py
@@ -28,7 +28,7 @@ class FeatureTwoSelector:
 
     def CheckAnswerCorrectness(self,request):
         print ("correct answer"+(request.get("result").get("parameters").get("correctAnswerID")))
-        print("chosen answer"+request.get("result").get("parameters").get("chosenAnswer"))
+        print("chosen answer"+str(request.get("result").get("parameters").get("chosenAnswer")))
         if request.get("result").get("parameters").get("correctAnswerID") == str(request.get("result").get(
                 "parameters").get("chosenAnswer")):
             return self.getRandomQuestion("Correct Answer :)")
