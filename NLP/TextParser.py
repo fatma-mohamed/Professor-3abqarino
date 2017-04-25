2edae116692f1fe87e1159ebfc5093d47b3f55a9
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-04-25 23:48:43 +0200

    Updated select & insert functions

--- NLP/TextParser.py
+++ NLP/TextParser.py
@@ -1,12 +1,10 @@
 from nltk.tokenize import word_tokenize
 
 class TextParser:
-    def __init__(self):
-        x =""
+
     def tokenize(self, input):
         list = word_tokenize(input)
         return list
-
 
     def removeStopWords(self, list):
         stopList = set(line.strip() for line in open('NLP/stoplist'))
