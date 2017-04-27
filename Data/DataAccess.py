f47cfdf9e82779a72acdae87e9bf5227b7096a90
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-04-27 08:18:53 +0200

    selectGifsRandom

--- Data/DataAccess.py
+++ Data/DataAccess.py
@@ -22,8 +22,8 @@ class DataAccess:
                 break
             conditions += (str(parameters[j]) + " = " + str(values[j]) + " " + str(operators[j]) + " ")
 
-        cur.execute('''SELECT ''' + cols_str + ''' FROM "''' + table_name + ''' WHERE ''' + conditions +
-                    '''" OFFSET floor(random()*(SELECT COUNT(*) FROM "''' + table_name + '''")) LIMIT 1''')
+        cur.execute('''SELECT ''' + cols_str + ''' FROM "''' + table_name + '''" WHERE ''' + conditions +
+                    ''' OFFSET floor(random()*(SELECT COUNT(*) FROM "''' + table_name + '''")) LIMIT 1''')
         rows = cur.fetchall()
         cur.close()
         print (rows)
