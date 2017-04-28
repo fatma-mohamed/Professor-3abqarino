7c3a4dba1ecfa933e01189c847c07c988ffdcf45
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-04-28 06:22:18 +0200

    Added Random Gifs

--- ResponseSelection/FeatureTwoSelector.py
+++ ResponseSelection/FeatureTwoSelector.py
@@ -1,38 +1,69 @@
-
-
 from ResponseSelection.ResponseSelector import ResponseSelector
 from Data import DataAccess
+from random import randint
 
 class FeatureTwoSelector:
     
-    def getRandomQuestion(self,answerFeedback = ""):
+    def getRandomQuestion(self,answerFeedback = "", imageURL = ""):
         row = DataAccess.DataAccess().selectRandom('''Questions_Answers''')
-        return {
-        "speech" : "",
-        "displayText": "",
-        "data": {},
-        "contextOut": [],
-        "source": "get-random-question",
-        "followupEvent":{
-            "name":"Question_Answers",
-            "data":{
-                "Question": row[1],
-                "A1": row[2],
-                "A2": row[3],
-                "A3": row[4],
-                "CA_ID": row[5],
-                "AnswerFeedback": answerFeedback
+        if imageURL == "":
+            return {
+                "speech": "",
+                "displayText": "",
+                "data": {},
+                "contextOut": [],
+                "source": "get-random-question",
+                "followupEvent": {
+                    "name": "Question_Answers",
+                    "data": {
+                        "Question": row[1],
+                        "A1": row[2],
+                        "A2": row[3],
+                        "A3": row[4],
+                        "CA_ID": row[5],
+                        "AnswerFeedback": answerFeedback
+                    }
                 }
             }
-        }
+        else:
+            return {
+                "speech": "",
+                "displayText": "",
+                "data": {},
+                "contextOut": [],
+                "source": "get-random-question",
+                "followupEvent": {
+                    "name": "Question_Answers",
+                    "data": {
+                        "Question": row[1],
+                        "A1": row[2],
+                        "A2": row[3],
+                        "A3": row[4],
+                        "CA_ID": row[5],
+                        "AnswerFeedback": answerFeedback,
+                        "imageURL" : imageURL
+                    }
+                }
+            }
 
     def CheckAnswerCorrectness(self,request):
         correctAnswer= request.get("correctAnswerID")
         chosenAnswer= str(request.get("chosenAnswer"))
+        randomNum = randint(0,19)
         if correctAnswer == chosenAnswer:
-            return self.getRandomQuestion("Correct Answer :)")
+            if randomNum < 10:
+                return self.getRandomQuestion("Correct Answer :)")
+            else:
+                d = DataAccess.DataAccess()
+                url = d.selectGifsRandom("Gifs" , ["url"] , ["tag"] , ["'correct'"], "")
+                return self.getRandomQuestion("Correct Answer :)", url)
         elif correctAnswer != chosenAnswer:
-            return self.getRandomQuestion("Wrong Answer :(")
+            if randomNum < 10:
+                return self.getRandomQuestion("Wrong Answer :(")
+            else:
+                d = DataAccess.DataAccess()
+                url = d.selectGifsRandom("Gifs" , ["url"] , ["tag"] , ["'incorrect'"], "")
+                return self.getRandomQuestion("Wrong Answer :(", url)
 
 
 
