b16be01c60870809467eff11e2f25dc0bcb1d300
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-05-08 01:17:46 +0200

    Cleaned F#1 code

--- Preprocessing/DataPreprocessing.py
+++ Preprocessing/DataPreprocessing.py
@@ -1,12 +1,7 @@
-from Data.Database import Database
-from Data.DataAccess import DataAccess
-from NLP.TextParser import TextParser
-from NLP.WordRecognizer import WordRecognizer
-
+from Data import *
 
 class DataPreprocessing:
-
-
+    @staticmethod
     def __run__(self, db):
         db.__createTables__()
 
@@ -18,92 +13,10 @@ class DataPreprocessing:
             "source": "create-database"
         }
 
-    @staticmethod
-    def insertAnswers_and_keywords():
-        db = Database()
-        db_access = DataAccess()
-        parser = TextParser()
-        recognizer = WordRecognizer()
-        file = open("Preprocessing/essay_questions.txt", "r")
-
-        while True:
-            line = file.readline()
-            if (line == ''):
-                print("no question!")
-                break
-            question = DataPreprocessing.removeSinqleQuotes(line.strip('-'))
-            line = file.readline()
-            if (line == ''):
-                print("no answer!")
-                break
-            answer = DataPreprocessing.removeSinqleQuotes(line.strip('\n'))
-            cols = ["answer"]
-            values = ["'" + answer + "'"]
-            db.insert("Answers", cols, values, "", "")
-
-            cols = ["id"]
-            parameters = ["answer"]
-            values = ["'" + answer + "'"]
-            answer_id = db_access.select("Answers", cols, parameters, values, "")
-
-            tokens = parser.tokenize(question)
-            keywords = parser.removeStopWords(tokens)
-            keywords_id = []
-            for k in keywords:
-
-                cols = ["id"]
-                parameters = ["keyword"]
-                values = [ "'" + k + "'"]
-                keyword_id = db_access.select("Keywords", cols, parameters,values,"")
-                if len(keyword_id) == 0:
-                    cols = ["keyword"]
-                    values = ["'" + k + "'"]
-                    conflict_fields = ["keyword"]
-                    db.insert("Keywords", cols, values, conflict_fields, "")
-
-                    cols = ["id"]
-                    parameters = ["keyword"]
-                    values = ["'" + k + "'"]
-                    id = db_access.select("Keywords", cols, parameters, values, "")
-                    keywords_id.append(id[0][0])
-                    synonyms = recognizer.getSynonym(k)
-                    for s in synonyms:
-                        cols = ["key_id", "synonym"]
-                        values = [str(id[0][0]) + ", '" + s + "'"]
-                        db.insert("Synonyms", cols, values, "", "")
-                else:
-                    keywords_id.append(keyword_id[0][0])
-            for i in keywords_id:
-                cols = ["answer_id", "keyword_id"]
-                values = [str(answer_id[0][0]) + "," + str(i)]
-                conflict_fields = ["answer_id", "keyword_id"]
-                db.insert("Answers_Keywords", cols, values, conflict_fields, "")
-
-    @staticmethod
-    def insertGifs():
-        db = Database()
-        file = open("Preprocessing/gifs.txt", "r")
-
-        while True:
-            line = file.readline()
-            if (line == ''):
-                print("EOF!")
-                break
-            arr = line.split(" ")
-            name = "'" + arr[0].strip("\n") + "'"
-            url = "'" + arr[1].strip("\n") + "'"
-            tag = "'" + arr[2].strip("\n") + "'"
-            db.insert("Gifs", ["Name", "Url" , "Tag"], [name,url,tag],"","")
-
-    @staticmethod
-    def removeSinqleQuotes(s):
-        res = s.replace("'", '"')
-        return res
-
-
+        ##rest of the preprocessing
 
     def insertQuestions_Answers(self):
-        f = open("Question_Answers.txt", 'r')
+        f = open("Preprocessing/Question_Answers.txt", 'r')
         i=0
         while True:
             Question = f.readline().rstrip()
