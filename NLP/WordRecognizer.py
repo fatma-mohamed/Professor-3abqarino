9ca71209310985369fe186dce095d0b917ee3d46
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-05-08 00:29:37 +0200

    Removed F#1 code from branch

--- NLP/WordRecognizer.py
+++ NLP/WordRecognizer.py
@@ -1,14 +1,2 @@
-from nltk.corpus import wordnet as wn
-from nltk import word_tokenize, pos_tag, ne_chunk
 class WordRecognizer:
-
-	def getSynonym(self, word):
-		list = set()
-		for w in wn.synsets(word):
-			list.add(w.lemmas()[0].name())
-		return list
-
-	def namedEntity(self, tokens):
-		tags = pos_tag(tokens)
-		ner = ne_chunk(tags)
-		return ner
+    dummy_data = "dummy_data"
