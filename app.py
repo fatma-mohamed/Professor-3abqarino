82047304508c04425df35634b29d1d6f38b2183d
Ibrahim Mostafa <Ibrahim Mostafa>
2017-05-11 01:58:30 +0200

    add html fle and render it as home and test it

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import urllib
 import json
 import os
 
-from flask import Flask
+from flask import Flask ,render_template
 from flask import request
 from flask import make_response
 from ResponseSelection import ResponseSelector, FeatureOneSelector, FeatureTwoSelector
