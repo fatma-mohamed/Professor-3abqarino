65f0817fd8369025d71404cef3e99f3d82248f13
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-05-15 22:31:53 +0200

    Gif update URLs

--- app.py
+++ app.py
@@ -41,6 +41,11 @@ def makeWebhookResult(req):
         question = (req.get("result")).get("resolvedQuery")
         responseSelector = FeatureOneSelector.FeatureOneSelector(question)
         return  responseSelector.getResult()
+    elif action == "gif":
+        db = Database.Database()
+        db.deleteDataGif()
+        p = DataPreprocessing.DataPreprocessing()
+        p.insertGifs()
     else:
         return {}
 
