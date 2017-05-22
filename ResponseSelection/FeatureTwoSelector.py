db782e46a5cc4c7f760a5d66073ec3211f670ca6
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-05-23 01:59:20 +0200

    test selectRandom for questions -- generalize it -- V1.3.1

--- ResponseSelection/FeatureTwoSelector.py
+++ ResponseSelection/FeatureTwoSelector.py
@@ -6,48 +6,49 @@ class FeatureTwoSelector:
     
     def getRandomQuestion(self,answerFeedback = "", imageURL = ""):
        # row = DataAccess.DataAccess().selectRandom('''Questions_Answers''')
-        row = DataAccess.DataAccess().selectGifsRandom("Questions_Answers",
+       rows = DataAccess.DataAccess().selectGifsRandom("Questions_Answers",
                                                        ["Question", "Answer_1", "Answer_2", "Answer_3", "Correct_AnswerID"],
                                                        [], [], "")
-        if imageURL == "":
-            return {
-                "speech": "",
-                "displayText": "",
-                "data": {},
-                "contextOut": [],
-                "source": "get-random-question",
-                "followupEvent": {
-                    "name": "Question_Answers",
-                    "data": {
-                        "Question": row[0],
-                        "A1": row[1],
-                        "A2": row[2],
-                        "A3": row[3],
-                        "CA_ID": row[4],
-                        "AnswerFeedback": answerFeedback
-                    }
-                }
-            }
-        else:
-            return {
-                "speech": "",
-                "displayText": "",
-                "data": {},
-                "contextOut": [],
-                "source": "get-random-question",
-                "followupEvent": {
-                    "name": "Question_Answers",
-                    "data": {
-                        "Question": row[1],
-                        "A1": row[2],
-                        "A2": row[3],
-                        "A3": row[4],
-                        "CA_ID": row[5],
-                        "AnswerFeedback": answerFeedback,
-                        "imageURL" : imageURL
-                    }
-                }
-            }
+       for row in rows:
+           if imageURL == "":
+               return {
+                   "speech": "",
+                   "displayText": "",
+                   "data": {},
+                   "contextOut": [],
+                   "source": "get-random-question",
+                   "followupEvent": {
+                       "name": "Question_Answers",
+                       "data": {
+                           "Question": row[0],
+                           "A1": row[1],
+                           "A2": row[2],
+                           "A3": row[3],
+                           "CA_ID": row[4],
+                           "AnswerFeedback": answerFeedback
+                       }
+                   }
+               }
+           else:
+               return {
+                   "speech": "",
+                   "displayText": "",
+                   "data": {},
+                   "contextOut": [],
+                   "source": "get-random-question",
+                   "followupEvent": {
+                       "name": "Question_Answers",
+                       "data": {
+                           "Question": row[1],
+                           "A1": row[2],
+                           "A2": row[3],
+                           "A3": row[4],
+                           "CA_ID": row[5],
+                           "AnswerFeedback": answerFeedback,
+                           "imageURL": imageURL
+                       }
+                   }
+               }
 
     def CheckAnswerCorrectness(self,request):
         correctAnswer= request.get("correctAnswerID")
