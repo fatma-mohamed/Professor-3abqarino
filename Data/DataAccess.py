3bd20024bc76e99c73b8579be186b6dc77c79555
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-05-23 02:36:09 +0200

    Gif trial

--- Data/DataAccess.py
+++ Data/DataAccess.py
@@ -26,8 +26,10 @@ class DataAccess:
                     '''" WHERE ''' + conditions + ''' ORDER BY RANDOM() limit 1;''')
         rows = cur.fetchall()
         cur.close()
-        print (rows)
-        print (rows[0])
+        print ("ROWS: ",rows)
+        l = rows[0]
+        print ("ROW :",l)
+        print("C: ", l[0])
         return rows[0]
 
     def select(self, table_name, cols, parameters, values , operators):
