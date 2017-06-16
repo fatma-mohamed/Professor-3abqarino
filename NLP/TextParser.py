8af0ab3838c07784bdc737eb43283470e7e4c498
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-06-17 01:58:09 +0200

    webSearch : remove spaces from query

--- NLP/TextParser.py
+++ NLP/TextParser.py
@@ -7,6 +7,6 @@ class TextParser:
         return list
 
     def removeStopWords(self, list):
-        stopList = set(line.strip() for line in open('stoplist'))
+        stopList = set(line.strip() for line in open('NLP/stoplist'))
         filtered_words = [word for word in list if word not in stopList]
         return filtered_words
