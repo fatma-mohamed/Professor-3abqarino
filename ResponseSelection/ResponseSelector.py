84ac738a48c55113c733ab5b5f21d372e8f0e072
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-06-18 16:43:11 +0200

    Test notify V1.8

--- ResponseSelection/ResponseSelector.py
+++ ResponseSelection/ResponseSelector.py
@@ -74,7 +74,7 @@ class ResponseSelector:
             current_time = datetime.datetime.now() - datetime.timedelta(hours=2)  # Convert to GMT (now - 2H)
             resultedTime = current_time - updated_time  # Didn't talk since...
             dayIndex = str(resultedTime).find("day")  # Get day index in result, -1 if not exist
-            if dayIndex > 0 and int(str(resultedTime)[:dayIndex-1]) >= 1:  # If day exists and number of days more than 2 ,, then notify user.
+            if(dayIndex == 0 or (dayIndex > 0 and int(str(resultedTime)[:dayIndex-1])) >= 2):  # If day exists and number of days more than 2 ,, then notify user.
                 conversationData = conversation.get("participants").get("data")
                 for participant in conversationData:
                     if participant.get("id") != config.page_id:  # Current participant isn't the page
