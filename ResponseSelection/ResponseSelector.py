778c66139ae2abdd12b1d0455705feba7402b969
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-06-18 16:55:33 +0200

    Test notify V1.10

--- ResponseSelection/ResponseSelector.py
+++ ResponseSelection/ResponseSelector.py
@@ -84,7 +84,7 @@ class ResponseSelector:
                         print "---------appp scode"
                         pageScopedID = DataAccess.DataAccess().select("User", ["Page_ScopedID"], ["App_ScopedID"], [appScopedID], "")
                         print "---------------- Page scoped iD----"
-                        print pageScopedID
+                        print pageScopedID[0][0]
                         print "---------------- Page scoped ID end"
                         if pageScopedID is not None:
                             ids.append(pageScopedID)
