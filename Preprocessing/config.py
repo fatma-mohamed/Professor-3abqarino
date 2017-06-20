19ceae71baa1ff648ff6f67d7874d7f3e11eaa23
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-06-20 18:49:57 +0200

    Test 2 days V1.0

--- Preprocessing/config.py
+++ Preprocessing/config.py
@@ -1,3 +1,4 @@
 ##FACEBOOK
 access_token = "EAAOSFv52vZAYBAFdFucXO27sKvzpn2V9lkwj9FoVOKOQ0GZCp13TiodlD1lPC8wDmN3xu4IM6mttuYPZAoP5GZAUoPK6KpI3tB5ZCgCe8BowZBpGnHW79abH0GOoKmDLGwQUVwVgZCZCGfBpaSKkzAtzDdlrbdO5uE04oJil2CYxcQZDZD"
 page_id = "225552284515947"
+graph_api_url = "https://graph.facebook.com/v2.9/"
