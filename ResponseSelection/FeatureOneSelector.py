8fbc4e6d25be400c291b576b57f857791a0370df
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-07-02 04:51:45 +0200

    retrieveSynonymID: add keywordIDs not only the synonyms

--- ResponseSelection/FeatureOneSelector.py
+++ ResponseSelection/FeatureOneSelector.py
@@ -79,7 +79,7 @@ class FeatureOneSelector():
         Da = DataAccess.DataAccess()
         for word in keywords:
             word =DataPreprocessing.removeSinqleQuotes(word)
-            w = "'" + word + "'"
+            word = "'" + word + "'"
             ids = Da.select("Keywords", ["id"], ["keyword"],[word],"")
             if len(ids)==0:
                 ids = Da.select("Synonyms", ["key_id"], ["synonym"],[word],"")
