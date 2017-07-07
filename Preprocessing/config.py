6055a5edefb336482e7dc85adfff38f19dbd3035
fatma-mohamed <fatma.mohmed.001@gmail.com>
2017-07-07 21:41:36 +0200

    Remove keys from config.py

---
+++ Preprocessing/config.py
@@ -0,0 +1,10 @@
+import os
+
+##FACEBOOK
+access_token = os.environ["access_token"]
+page_id = os.environ["page_id"]
+graph_api_url = "https://graph.facebook.com/v2.9/"
+
+##Google Custom Search Engine
+api_key = os.environ["cse_api_key"]
+engine_id = os.environ["engine_id"]
