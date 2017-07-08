5364de42a2137f13b0b5f752e67ee338edd7877d
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-07-09 00:17:01 +0200

    Test Notifications with Gif V1.0

--- ResponseSelection/ResponseSelector.py
+++ ResponseSelection/ResponseSelector.py
@@ -72,7 +72,7 @@ class ResponseSelector:
             current_time = datetime.datetime.now() + datetime.timedelta(hours=2)  # Convert to GMT (now + (-2)H)
             resultedTime = current_time - updated_time  # Didn't talk since...
             dayIndex = str(resultedTime).find("day")  # Get day index in result, -1 if not exist
-            if dayIndex > 0 and int(str(resultedTime)[:dayIndex-1]) >= 2:  # If day exists and number of days more than 2 ,, then notify user.
+            if True or dayIndex > 0 and int(str(resultedTime)[:dayIndex-1]) >= 2:  # If day exists and number of days more than 2 ,, then notify user.
                 conversationData = conversation.get("participants").get("data")
                 for participant in conversationData:
                     if participant.get("id") != config.page_id:  # Current participant isn't the page
@@ -80,8 +80,9 @@ class ResponseSelector:
                         pageScopedID = DataAccess.DataAccess().select("User", ["Page_ScopedID"], ["App_ScopedID"], [appScopedID], "")
                         if pageScopedID != []:
                             print conversation.get("participants").get("data")[0].get("name")
-                            ids.append(pageScopedID[0][0])
-                            break
+                            if pageScopedID[0][0] == 1194774927301799:
+                                ids.append(pageScopedID[0][0])
+                                break
         return ids
 
     def getConversations(self):
@@ -107,7 +108,7 @@ class ResponseSelector:
             if(attachment != None): #If there's an attachment, send it before the message itself
                 if content[0][2] == "GIF":
                     attachedGif = DataAccess.DataAccess().selectRandom("Gifs", ["Url"], ["Gif_Tag"], ["'" + attachment + "'"], "")
-                    requestJSON = {'recipient': '{"id": ' + userID + '}',
+                    requestJSON = {'recipient': '{"id": ' + str(userID) + '}',
                                    "message": '{ "attachment": { "type":"image", "payload":{ "url":' + '"' + attachedGif[0][0] + '"' + ' } } }'
                                    }
                     r = requests.post(url, data=requestJSON, headers={'Content-type': 'application/json'})
